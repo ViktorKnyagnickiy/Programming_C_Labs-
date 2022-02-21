9619ba7fd36a5ab2abdd6a4327f26bd17ca6f1b0
ViktorKnyagnickiy <99738666+ViktorKnyagnickiy@users.noreply.github.com>
2022-02-21 09:46:16 +0200

    Add files via upload

---
+++ Lab02/lab0118/lab0118/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_CTYPE, "ukr");
+	double  x1, y1, x2, y2, s1, s2;
+	cout << "Введiть координати точки x1:" << endl;
+	cin >> x1;
+	cout << "Введiть координати точки y1:" << endl;
+	cin >> y1;
+	cout << "Введiть координати точки x2:" << endl;
+	cin >> x2;
+	cout << "Введiть координати точки y2:" << endl;
+	cin >> y2;
+	s1 = sqrt(x1 * x1 + y1 * y1);
+	s2 = sqrt(x2 * x2 + y2 * y2);
+
+	if (s1 > s2) {
+		cout << "ПЕРША точка знаходиться ДАЛI вiд початку координат.";
+	}else if(s1 == s2) {
+			cout<< "Точки знаходяться вiдносно координат однаково.";
+	}
+	else {
+		cout << "ДРУГА точка знаходиться ДАЛI вiд початку координат.";
+	}
+
+
+
+	
+	
+	return 0;
+}
+	
---
+++ Lab02/lab0213/lab0213/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+int main() {
+	setlocale(LC_CTYPE, "ukr");
+
+
+
+	return 0;
+}
