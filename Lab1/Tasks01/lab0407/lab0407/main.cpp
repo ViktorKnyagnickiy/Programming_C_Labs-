69f2be44da906b0b6fb31936800e002b61a2e0df
victor <kn1b21.kniahnytskyi@kpnu.edu.ua>
2022-04-12 17:14:12 +0300

    Lab1

---
+++ Lab1/Tasks01/lab0407/lab0407/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_CTYPE, "ukr");
+	double y, t, a,s0=120,s; //де y це швидкість, t - час, а - прискорення, s0 - відома початкова відстань, s- відстань, яку ми шукаємо
+	cout << "Введiть швидкiсть : ";
+	cin >> y;
+	cout << "Введiть час : ";
+	cin >> t;
+	cout << "Введiть прискорення : ";
+	cin >> a;
+	
+
+	s = y * (s0 / y) + (a * pow(t, 2) / 2);
+
+	cout << "Вiдстань кiнцева : " << s;
+ 
+
+
+
+	return 0;
+}
+
